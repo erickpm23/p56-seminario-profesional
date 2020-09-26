41807a110820cfc6cf4d234ac0802ab43942d9dd
Erick <episcom@est.ups.edu.ec>
2020-09-25 20:17:36 -0500

    Estructuras Repetitivas.

--- code/javascript/06_estructuras_repetitivas.js
+++ code/javascript/06_estructuras_repetitivas.js
@@ -10,8 +10,8 @@ const DECREMENTAR_PESO = 2
 const aumentarPeso = (objeto) => objeto.peso += INCREMENTAR_PESO
 const disminuyePeso = (objeto) => objeto.peso -= DECREMENTAR_PESO
 
-const comeMucho = () =>Math.random() < 0.2
-const realizaDeporte = () =>Math.random() < 0.8
+const comeMucho = () =>Math.random() < 0.7
+const realizaDeporte = () =>Math.random() < 0.2
 
 const META = persona.peso - 10
 
