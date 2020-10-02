7f5d4aae0d1839b2046dccebae36294fcf55186d
Erick <episcom@est.ups.edu.ec>
2020-10-01 22:26:58 -0500

    Uso de los archivos config.js & db.js

--- code/node-project/server.js
+++ code/node-project/server.js
@@ -2,15 +2,19 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const response = require('./network/response')
 
+const config = require('./config')
+const db = require('./db')
 const router = require('./network/routes')
+const { host } = require('./config')
+
+db( config.dbUrl )
 
 var app = express()
 app.use( bodyParser.json() )
 app.use( bodyParser.urlencoded( {extended:false} ) )
 router( app )
 
+app.use( config.publicRoute, express.static('public') )
 
-app.use( '/', express.static('public') )
-
-app.listen( 5000 )
-console.log( 'La aplicacion esta escuchando en http://localhost:5000' )
+app.listen( config.port )
+console.log( `La aplicacion esta escuchando en ${config.host}:${config.port} ${config.publicRoute}` )
